dfa5685dab712383f78f85db14f7bb53f99c4863
albertdwag <albertcesar463@gmail.com>
2023-04-18 18:29:54 -0300

    adding a magnetic to the coin.

--- Assets/Scripts/Ebac/Items/DestructableItem/DestructableItemBase.cs
+++ Assets/Scripts/Ebac/Items/DestructableItem/DestructableItemBase.cs
@@ -8,6 +8,9 @@ public class DestructableItemBase : MonoBehaviour
     public HealthBase healthBase;
     public float shakeDuration = .1f;
     public int shakeForce = 5;
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
 
     private void OnValidate()
     {
@@ -23,5 +26,28 @@ public class DestructableItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(shakeDuration, Vector3.up / 2, shakeForce);
+        DropGroupOfCoins();
+    }
+
+    [NaughtyAttributes.Button]
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, .2f).SetEase(Ease.OutBack).From();
+    }
+
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for (int i = 0; i < dropCoinsAmount; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
